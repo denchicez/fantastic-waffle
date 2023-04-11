5ef338f49b6953e42e13b3a0ba21d1435b4f8826
denchicez <denchicez@gmail.com>
2023-04-11 14:45:41 +0300

    init

---
+++ Test/Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Денис Жимоедов on 11.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
